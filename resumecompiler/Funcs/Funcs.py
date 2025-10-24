daba6f72040d7206de4d1ee6105d99dd664cb326
raphaellith <raphaellith@gmail.com>
2025-10-24 21:57:42 +0100

    refactored Funcs.py

--- resumecompiler/Funcs/Funcs.py
+++ resumecompiler/Funcs/Funcs.py
@@ -7,24 +7,18 @@ def strip_strings(strings: list[str]) -> list[str]:
     :param strings: A list of strings.
     :return: A list of strings that have been stripped. Strings that contain only whitespaces are removed.
     """
-    result = []
 
-    for s in strings:
-        stripped = s.strip()
-        if stripped:
-            result.append(stripped)
-
-    return result
+    return list(filter(lambda s: s, map(str.strip, strings)))
 
 
 def take_fixed_num_of_inputs_with_defaults(inputs: list, defaults: list) -> list:
     """
     :param inputs: A list of inputs.
     :param defaults: A list of default values.
-    :return: A list with the same length as default.
-    If len(inputs) < len(default), the original list is padded using the default values at the corresponding indices.
-    If len(inputs) == len(default), the original list is returned with no changes made.
-    If len(inputs) > len(default), the original list is cut off early to match the length of the default list.
+    :return: A list of inputs with the same length as default.
+    If len(inputs) < len(default), the input list is padded using the default values at the corresponding indices.
+    If len(inputs) == len(default), the input list is returned with no changes made.
+    If len(inputs) > len(default), the input list is cut off early to match the length of the default list.
     """
 
     return [inputs[i] if i < len(inputs) else default for i, default in enumerate(defaults)]
@@ -36,9 +30,9 @@ def take_fixed_num_of_inputs_with_same_default(inputs: list, n: int, default: An
     :param n: The number of inputs to accept.
     :param default: The default input value to use for padding.
     :return: A list of n inputs.
-    If len(inputs) < n, the original list is padded using the default value until its length is n.
-    If len(inputs) == n, the original list is returned with no changes made.
-    If len(inputs) > n, a list containing the first n inputs in the original list is returned.
+    If len(inputs) < n, the input list is padded using the default value until its length is n.
+    If len(inputs) == n, the input list is returned with no changes made.
+    If len(inputs) > n, a list containing the first n strings in the input list is returned.
     """
 
     return [inputs[i] if i < len(inputs) else default for i in range(n)]
@@ -49,9 +43,9 @@ def take_fixed_num_of_input_strings(inputs: list[str], n: int) -> list[str]:
     :param inputs: A list of input strings.
     :param n: The number of input strings to accept.
     :return: A list of n strings.
-    If len(inputs) < n, the original list is padded until its length is n.
-    If len(inputs) == n, the original list is returned with no changes made.
-    If len(inputs) > n, a list containing the first n strings in the original list is returned.
+    If len(inputs) < n, the input list is padded with empty strings until its length is n.
+    If len(inputs) == n, the input list is returned with no changes made.
+    If len(inputs) > n, a list containing the first n strings in the input list is returned.
     """
 
     return take_fixed_num_of_inputs_with_same_default(inputs, n, "")
@@ -59,9 +53,10 @@ def take_fixed_num_of_input_strings(inputs: list[str], n: int) -> list[str]:
 
 def create_and_write_file(file_path: Path, contents: str):
     """
+    Creates the file (along with all intermediate directories) and writes the specified contents onto the file.
     :param file_path: A file path.
     :param contents: Contents to be written onto the file.
-    :return: Creates the file (along with all intermediate directories) and writes the specified contents onto the file.
+    :return:
     """
     # Create parent directories if they don't exist
     file_path.parent.mkdir(parents=True, exist_ok=True)
