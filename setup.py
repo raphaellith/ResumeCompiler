e2e6226960c0419e760c3c7a9fc973afc19c1d48
raphaellith <raphaellith@gmail.com>
2025-10-26 21:19:04 +0000

    updated version num

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='resumecompiler',
-    version='0.12',
+    version='0.13',
     packages=find_packages(),
     include_package_data=True,
     package_data={'': ['preamble.tex']},
