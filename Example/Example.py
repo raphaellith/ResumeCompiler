9677fae214f16e52f88bbbf5ae401ee76be05080
raphaellith <raphaellith@gmail.com>
2026-04-21 22:37:44 +0800

    cleanup: removed pypi-related files

--- Example/Example.py
+++
@@ -1,6 +0,0 @@
-from resumecompiler.Enums.Font import Font
-from resumecompiler import ResumeCompiler
-
-# To run the compiler with live reload
-compiler = ResumeCompiler("example-src", "example-dist")
-compiler.run_with_live_reload(font=Font.TIMES_NEW_ROMAN)
