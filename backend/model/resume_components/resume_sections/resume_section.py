a9ae2a993ccc648fde8c2d0bb13328e7dfac0d2c
raphaellith <raphaellith@gmail.com>
2026-06-10 22:51:38 +0800

    chore: added TODO

--- backend/model/resume_components/resume_sections/resume_section.py
+++ backend/model/resume_components/resume_sections/resume_section.py
@@ -3,6 +3,7 @@ from abc import ABC
 from backend.model.resume_components.resume_component import ResumeComponent
 
 
+# TODO: Refactor - Add ResumeSectionWithItems class from which ToolsetSection and OrganisationalSection inherit?
 class ResumeSection(ResumeComponent, ABC):
     def __init__(self, heading: str):
         super().__init__()
