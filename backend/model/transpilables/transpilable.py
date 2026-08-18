4c378f9dd462a23be193ec5be52b02f129934296
raphaellith <raphaellith@gmail.com>
2026-08-18 23:15:57 +0800

    docs: added docstrings to transpilable.py

--- backend/model/transpilables/transpilable.py
+++ backend/model/transpilables/transpilable.py
@@ -5,26 +5,31 @@ from abc import ABC, abstractmethod
 class Transpilable(ABC):
     def __init__(self):
         """
-        A Transpilable is an abstract base class that defines the interface of any part of a resume that can be compiled
-        to LaTeX.
+        Initializes a Transpilable, the abstract base class for resume parts that are compilable to LaTeX and XML.
         """
         pass
 
     @abstractmethod
     def to_latex(self) -> str:
         """
-        :return: The LaTeX code representation of this resume component.
+        Renders this Transpilable as LaTeX code.
+        :return: The LaTeX code representation of this Transpilable.
         """
         pass
 
     @abstractmethod
     def to_xml_element(self) -> ElementTree.Element:
         """
-        :return: The XML representation of this resume component.
+        Renders this Transpilable as an XML element.
+        :return: The XML representation of this Transpilable.
         """
         pass
 
     def to_xml_string(self) -> str:
+        """
+        Serializes this Transpilable to an indented XML string.
+        :return: The XML string representation of this Transpilable.
+        """
         xml_element = self.to_xml_element()
         ElementTree.indent(xml_element, space="  ")
         return ElementTree.tostring(xml_element, encoding="unicode")
