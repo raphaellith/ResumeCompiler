397adb8856defe1b5989375e6ca3ef4bb110c1ef
raphaellith <raphaellith@gmail.com>
2026-08-18 23:00:36 +0800

    docs: added docstrings to resume_component.py

--- backend/model/transpilables/resume_components/resume_component.py
+++ backend/model/transpilables/resume_components/resume_component.py
@@ -5,4 +5,7 @@ from backend.model.transpilables.transpilable import Transpilable
 
 class ResumeComponent(Transpilable, ABC):
     def __init__(self):
+        """
+        Initializes a ResumeComponent, the abstract base class for all resume components.
+        """
         super().__init__()
