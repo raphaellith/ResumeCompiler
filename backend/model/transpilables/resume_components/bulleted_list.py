8bcc73fcb8a04009c4ff36a4bb5fa569f70a9391
raphaellith <raphaellith@gmail.com>
2026-08-18 22:59:28 +0800

    docs: added docstrings to bulleted_list.py

--- backend/model/transpilables/resume_components/bulleted_list.py
+++ backend/model/transpilables/resume_components/bulleted_list.py
@@ -7,11 +7,27 @@ from backend.model.transpilables.resume_components.resume_component import Resum
 
 class BulletedList(ResumeComponent):
     def __init__(self, ul_tag: Tag):
+        """
+        Initializes a BulletedList component from a <ul> tag.
+        :param ul_tag: The <ul> tag whose <li> children make up the list items.
+        """
         super().__init__()
         self.li_tags: list[Tag] = ul_tag.find_all("li")
 
     def to_latex(self) -> str:
+        """
+        Converts the bulleted list to a LaTeX itemize environment.
+        :return: The LaTeX code representation of this list.
+        :raises ValueError: If a list item contains a tag other than <b> or <i>.
+        """
+
         def get_page_element_as_latex(element: PageElement) -> str:
+            """
+            Recursively converts a single page element to LaTeX, applying <b> and <i> formatting.
+            :param element: The page element (text or tag) to convert.
+            :return: The LaTeX code representation of the element.
+            :raises ValueError: If the element is an unsupported tag or a non-text, non-tag element.
+            """
             if isinstance(element, NavigableString):
                 return element.string
 
@@ -35,6 +51,10 @@ class BulletedList(ResumeComponent):
         return result
 
     def to_xml_element(self) -> ElementTree.Element:
+        """
+        Builds an XML element representing this bulleted list.
+        :return: A <bulleted-list> element containing one <list-item> element per list item.
+        """
         container_element = ElementTree.Element("bulleted-list")
 
         for li_tag in self.li_tags:
