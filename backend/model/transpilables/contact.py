fe564865f11e1a6562f7025b4874bc17c4642d93
raphaellith <raphaellith@gmail.com>
2026-08-20 22:17:15 +0800

    docs: added docstrings

--- backend/model/transpilables/contact.py
+++ backend/model/transpilables/contact.py
@@ -16,6 +16,10 @@ class Contact(Transpilable):
         self.link: Optional[str] = link
 
     def to_latex(self) -> str:
+        """
+        Converts the contact to LaTeX, including the hyperlink if present.
+        :return: The LaTeX code representation of this contact.
+        """
         display_as_latex = Transpilable.escape_for_latex(self.display)
 
         if not self.link:
@@ -25,6 +29,10 @@ class Contact(Transpilable):
         return r"\href{" + display_as_latex + r"}{\underline{" + link_as_latex + "}}"
 
     def to_xml_element(self) -> ElementTree.Element:
+        """
+        Builds an XML element representing this contact.
+        :return: A <contact> element with the display text and an optional link attribute.
+        """
         contact_element = ElementTree.Element("contact")
         contact_element.text = self.display
 
