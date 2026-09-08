16a9117ebed69c17faaa3977b7e8f8f74c17a509
raphaellith <raphaellith@gmail.com>
2026-09-08 20:58:36 +0800

    refactor: added import for global var 'React'

--- frontend/resumecompiler/src/App.tsx
+++ frontend/resumecompiler/src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { isTauri } from "@tauri-apps/api/core";
